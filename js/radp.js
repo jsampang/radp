fc50bda971f5d0795b7f1debb0f9a57d3b114fb3
Joven Sampang <jovenrowboat@Pro-Late-2025.T-mobile.com>
2018-03-13 13:45:43 -0700

    aa

--- js/radp.js
+++ js/radp.js
@@ -172,52 +172,19 @@ $(document).ready(function () {
 	});
 });
 
-		$(function () {
-			$('#error').click(function () {
-				// make it not dissappear
-				toastr.error("Error", "Page Title Here", {});
-			});
-			$('#info').click(function () {
-				// title is optional
-				toastr.info("Info Message", "Title");
-			});
-			$('#warning').click(function () {
-				toastr.warning("Warning");
-			});
-			$('#success').click(function () {
-				toastr.success("Success");
-			});
-		});
-
-/* tabs 
-
-		$(".nav-tabs").on("click", "a", function (e) {
-        e.preventDefault();
-        if (!$(this).hasClass('add-contact')) {
-            $(this).tab('show');
-        }
-    })
-    .on("click", "span", function () {
-        var anchor = $(this).siblings('a');
-        $(anchor.attr('href')).remove();
-        $(this).parent().remove();
-        $(".nav-tabs li").children('a').first().click();
-    });
-
-$('.add-contact').click(function (e) {
-    e.preventDefault();
-    var id = $(".nav-tabs").children().length; //think about it ;)
-    var tabId = 'contact_' + id;
-    $(this).closest('li').before('<li class="nav-item" ><a class="nav-link" href="#contact_' + id + '"> New Tab <span> x </span></a> </li>');
-    $('.tab-content').append('<div class="tab-pane" id="' + tabId + '">Contact Form: New Contact ' + id + '</div>');
-   $('.nav-tabs li:nth-child(' + id + ') a').click();
+$(function () {
+	$('#error').click(function () {
+		// make it not dissappear
+		toastr.error("Error", "Page Title Here", {});
+	});
+	$('#info').click(function () {
+		// title is optional
+		toastr.info("Info Message", "Title");
+	});
+	$('#warning').click(function () {
+		toastr.warning("Warning");
+	});
+	$('#success').click(function () {
+		toastr.success("Success");
+	});
 });
-
-
-$('.content-link').on('click', function () {
-	$('.content-link').removeClass('selected');
-	$(this).addClass('selected');
-});
-
-
-*/
